ba4e84352583b87180830dbe443ab54bf37832f4
Ryan Mitcham <ryanmitcham512@gmail.com>
2023-05-31 14:34:39 +0100

    Searching
    To execute a search query and display the results in a table

--- main.py
+++ main.py
@@ -5,8 +5,24 @@ import psycopg2
 root = tk.Tk()
 root.title("Documentation Database Interface")
 
+photo = tk.PhotoImage(file="/home/ryan/Documents/GitHub/DocumentationDatabase/search.png")
+root.iconphoto(False, photo)
+
 frame = ttk.Frame(root, padding=10)
 frame.grid()
+
+class Table:   
+    def __init__(self,root, data):   
+        # code for creating table
+        clear_frame()
+        ttk.Button(frame, text="<- Search", width=160, command=search_screen).grid(row=0, column=0, columnspan=4)
+        ttk.Label(frame, text="Results...").grid(row=1, column=0, columnspan=4)
+        for i in range(len(data)):
+            for j in range(len(data[i])):          
+                self.e = ttk.Entry(root, width=40)
+                self.e.grid(row=i+2, column=j)
+                self.e.insert(tk.END, data[i][j])
+                self.e.configure(state="readonly")
 
 # Connect to postgreSQL database
 def sql_connect():
@@ -34,8 +50,10 @@ def clear_frame():
         widgets.destroy()
 
 def search(query):
-    print(query)
+    cur.execute(f"SELECT links.name, link, type, languages.name FROM languages, links WHERE languages.langid = links.langid AND links.name LIKE '%{query}%';")
+    #cur.execute("SELECT * FROM links")
     search_screen()
+    Table(frame, cur.fetchall())
     
 def new_lang(name):
     global languages
@@ -50,18 +68,20 @@ def new_link(name, link, langname, linktype):
         if lang[1] == langname:
             langid = lang[0]
             break
-    cur.execute("INSERT INTO links (name, link, type, langid) VALUES (%s, %s, %s, %s);", (name, link, len(linktype) > 0, langid))
+    cur.execute("INSERT INTO links (name, link, type, langid) VALUES (%s, %s, %s, %s);", (name.strip(), link.strip(), len(linktype) > 0, langid))
     conn.commit()
     new_screen()
     
 def search_screen():
     clear_frame()
-    ttk.Label(frame, text="Search Entries").grid(column=0, row=0, columnspan=2)
     
-    search_query = ttk.Entry(frame, width=40)
-    search_query.grid(column=0, row=1)
+    ttk.Button(frame, text="Add New ->", command=new_screen, width=80).grid(column=0, row=0, columnspan=2)
+    ttk.Label(frame, text="Search Entries").grid(column=0, row=1, columnspan=2)
     
-    ttk.Button(frame, text="Search", command=lambda: search(search_query.get())).grid(column=1, row=1)
+    search_query = ttk.Entry(frame, width=60)
+    search_query.grid(column=0, row=2)
+    
+    ttk.Button(frame, text="Search", command=lambda: search(search_query.get()), width=20).grid(column=1, row=2)
     
 def new_screen():
     clear_frame()
@@ -96,8 +116,10 @@ def new_screen():
     
     ttk.Button(frame, text="Add", command=lambda: new_link(name.get(), link.get(), value_inside.get(), linktype.state())).grid(column=0, row=7, columnspan=2)
 
+    ttk.Button(frame, text="<- Search", command=search_screen, width=80).grid(column=0, row=8, columnspan=2)
+
 conn, cur = sql_connect()
 languages = get_lang()    
 
-new_screen()
+search_screen()
 root.mainloop()
