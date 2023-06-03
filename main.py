17c7b8afe85f7a4a718fc9628a9882b85e4dfdea
Ryan Mitcham <83707757+RM220507@users.noreply.github.com>
2023-06-03 17:41:12 +0100

    Update main.py
    Remove personal passwords

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def sql_connect():
   
     # connect to the PostgreSQL server
     print('Connecting to the PostgreSQL database...')
-    conn = psycopg2.connect(host="192.168.1.179", database="documentation", user="postgres", password="xm32BAZRP")
+    conn = psycopg2.connect(host="<HOSTNAME>", database="documentation", user="postgres", password="<PASSWORD>")
           
     # create a cursor
     cur = conn.cursor()
