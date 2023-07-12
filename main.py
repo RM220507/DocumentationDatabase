40d7c901f98c509c6ee3084a14d5de96f35595af
Ryan Mitcham <ryanmitcham512@gmail.com>
2023-07-12 19:48:58 +0100

    Beginning to recreate interface

--- main.py
+++ main.py
@@ -1,132 +1,68 @@
-import tkinter as tk
-from tkinter import ttk
-import psycopg2
-from configparser import ConfigParser
+from tkinter import *
 
-root = tk.Tk()
+# setup window
+root = Tk()
 root.title("Documentation Database Interface")
+root.config(bg="skyblue")
 
-photo = tk.PhotoImage(file="search.png")
-root.iconphoto(False, photo)
+#photo = PhotoImage(file="search.png")
+#root.iconphoto(False, photo)
 
-frame = ttk.Frame(root, padding=10)
-frame.grid()
 
-class Table:   
-    def __init__(self,root, data):   
-        # code for creating table
-        clear_frame()
-        ttk.Button(frame, text="<- Search", width=160, command=search_screen).grid(row=0, column=0, columnspan=4)
-        ttk.Label(frame, text="Results...").grid(row=1, column=0, columnspan=4)
-        for i in range(len(data)):
-            for j in range(len(data[i])):          
-                self.e = ttk.Entry(root, width=60)
-                self.e.grid(row=i+2, column=j)
-                self.e.insert(tk.END, data[i][j])
-                self.e.configure(state="readonly")
+# setup the left and right frames to hold the items
+left_frame = Frame(root, width=200, height=400, bg="grey")
+left_frame.grid(row=0, column=0, padx=10, pady=10)
 
-def get_config():
-    parser = ConfigParser()
-    parser.read("client_conf.ini")
-    
-    return dict(parser.items("postgresql"))
+right_frame = Frame(root, width=650, height=400, bg="grey")
+right_frame.grid(row=0, column=1, padx=10, pady=10)
 
-# Connect to postgreSQL database
-def sql_connect():
-  
-    # connect to the PostgreSQL server
-    print('Connecting to the PostgreSQL database...')
-    conf = get_config()
-    conn = psycopg2.connect(**conf)
-          
-    # create a cursor
-    cur = conn.cursor()
-          
-    # display database version
-    cur.execute('SELECT version()')
-    db_version = cur.fetchone()
-    print(f"Connected to PostgreSQL database with version {db_version}")
-    
-    return conn, cur
+# add the items in the left frame - SEARCH and ADD NEW
+# SEARCH FRAME
+search_frame = Frame(left_frame, width=175, height=200, bg="pink")
+search_frame.grid(row=0, column=0, padx=5, pady=5)
 
-def get_lang():
-    cur.execute("SELECT * FROM languages")
-    return cur.fetchall()
+#search label
+Label(search_frame, text="Search", bg="pink").grid(row=0, column=0, columnspan=2, padx=5, pady=5)
 
-def clear_frame():
-    for widgets in frame.winfo_children():
-        widgets.destroy()
+# entry to search NAME
+search_query = Entry(search_frame, width=30)
+search_query.grid(row=1, column=0, columnspan=2)
 
-def search(query):
-    cur.execute(f"SELECT links.name, link, type, languages.name FROM languages, links WHERE languages.langid = links.langid AND links.name LIKE '%{query}%';")
-    #cur.execute("SELECT * FROM links")
-    Table(frame, cur.fetchall())
-    
-def new_lang(name):
-    global languages
-    cur.execute("INSERT INTO languages (name) VALUES (%s);", (name,))
-    conn.commit()
-    languages = get_lang()
-    new_screen()
+# checkbox to enable/disable advanced searching
+search_advanced_check = BooleanVar(search_frame)
+Checkbutton(search_frame, text="Advanced Search?", variable=search_advanced_check, bg="pink").grid(row=2, column=0, columnspan=2, padx=5, pady=5)
 
-def new_link(name, link, langname, linktype):
-    langid = 0
-    for lang in languages:
-        if lang[1] == langname:
-            langid = lang[0]
-            break
-    cur.execute("INSERT INTO links (name, link, type, langid) VALUES (%s, %s, %s, %s);", (name.strip(), link.strip(), len(linktype) > 0, langid))
-    conn.commit()
-    new_screen()
-    
-def search_screen():
-    clear_frame()
-    
-    ttk.Button(frame, text="Add New ->", command=new_screen, width=80).grid(column=0, row=0, columnspan=2)
-    ttk.Label(frame, text="Search Entries").grid(column=0, row=1, columnspan=2)
-    
-    search_query = ttk.Entry(frame, width=60)
-    search_query.grid(column=0, row=2)
-    
-    ttk.Button(frame, text="Search", command=lambda: search(search_query.get()), width=20).grid(column=1, row=2)
-    
-def new_screen():
-    clear_frame()
-    
-    ttk.Label(frame, text="Add New Language").grid(column=0, row=0, columnspan=2)
-    
-    lang_name = ttk.Entry(frame, width=40)
-    lang_name.grid(column=0, row=1)
-    
-    ttk.Button(frame, text="Add", width=40, command=lambda: new_lang(lang_name.get())).grid(column=1, row=1)
-    
-    
-    ttk.Label(frame, text="Add New Link").grid(column=0, row=2, columnspan=2)
-    
-    ttk.Label(frame, text="Name").grid(column=0, row=3)
-    name = ttk.Entry(frame, width=40)
-    name.grid(column=1, row=3)
-    
-    ttk.Label(frame, text="Link").grid(column=0, row=4)
-    link = ttk.Entry(frame, width=40)
-    link.grid(column=1, row=4)
-    
-    ttk.Label(frame, text="Language").grid(column=0, row=5)
-    value_inside = tk.StringVar(frame)
-    value_inside.set("Python")
-    option = ttk.OptionMenu(frame, value_inside, *[lang[1] for lang in languages])
-    option.grid(column=1, row=5)
-    
-    ttk.Label(frame, text="Docs/Tutorial").grid(column=0, row=6)
-    linktype = ttk.Checkbutton(frame)
-    linktype.grid(column=1, row=6)
-    
-    ttk.Button(frame, text="Add", command=lambda: new_link(name.get(), link.get(), value_inside.get(), linktype.state())).grid(column=0, row=7, columnspan=2)
+# checkbox and option menu to allow type searching
+search_advanced_type_check = BooleanVar(search_frame)
+Checkbutton(search_frame, text="Type", variable=search_advanced_type_check, bg="pink").grid(row=3, column=0, padx=5, pady=5)
 
-    ttk.Button(frame, text="<- Search", command=search_screen, width=80).grid(column=0, row=8, columnspan=2)
+search_advanced_type = StringVar(search_frame)
+type_options = ["Documentation", "Tutorial", "Datasheet", "Purchase Link"]
+search_advanced_type_menu = OptionMenu(search_frame, search_advanced_type, *type_options)
+search_advanced_type_menu.grid(row=3, column=1, padx=5, pady=5)
+search_advanced_type_menu.config(width=15)
 
-conn, cur = sql_connect()
-languages = get_lang()    
+# checkbox and option menu to allow language searching
+search_advanced_lang_check = BooleanVar(search_frame)
+Checkbutton(search_frame, text="Lang", variable=search_advanced_lang_check, bg="pink").grid(row=4, column=0, padx=5, pady=5)
 
-search_screen()
+search_advanced_lang = StringVar(search_frame)
+lang_options = ["Python", "CSS", "HTML", "N/A"]
+search_advanced_lang_menu = OptionMenu(search_frame, search_advanced_lang, *lang_options)
+search_advanced_lang_menu.grid(row=4, column=1, padx=5, pady=5)
+search_advanced_lang_menu.config(width=15)
+
+# checkbox and entry to allow LinkID searching
+search_advanced_id_check = BooleanVar(search_frame)
+Checkbutton(search_frame, text="ID", variable=search_advanced_id_check, bg="pink").grid(row=5, column=0, padx=5, pady=5, sticky="W")
+
+search_advanced_id = Entry(search_frame, width=20)
+search_advanced_id.grid(row=5, column=1, padx=5, pady=5, sticky="E")
+
+# search button
+Button(search_frame, text="Search", width=25).grid(row=6, column=0, columnspan=2, padx=5, pady=5)
+
+# ADD NEW FRAME
+pass
+
 root.mainloop()
