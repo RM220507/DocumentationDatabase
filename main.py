cad3a20eb02118b583ad4fd7ff73619dd0077dd3
Ryan Mitcham <ryanmitcham512@gmail.com>
2023-07-21 11:17:56 +0100

    Use customtkinter for better GUI

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from tkinter import *
-from tkinter import ttk
+import customtkinter as ctk
 import mysql.connector
 import json
-from betteroptionmenu import BetterOptionMenu
 from table import Table
 from pymysql.converters import escape_string
 
@@ -76,26 +75,21 @@ def refresh_menus():
     result = cursor.fetchall()
     types = [val[0] for val in result]
 
-    search_advanced_type_menu.DeleteOptions()
-    search_advanced_type_menu.AddOptions(types)
-
-    add_type_menu.DeleteOptions()
-    add_type_menu.AddOptions(types)
+    search_advanced_type_menu.configure(values=types)
+    add_type_menu.configure(values=types)
 
     cursor.execute("SELECT LangName FROM Languages")
     result = cursor.fetchall()
     languages = [val[0] for val in result]
 
-    search_advanced_lang_menu.DeleteOptions()
-    search_advanced_lang_menu.AddOptions(languages)
+    search_advanced_lang_menu.configure(values=languages)
+    add_lang_menu.configure(values=languages)
 
-    add_lang_menu.DeleteOptions()
-    add_lang_menu.AddOptions(languages)
+ctk.set_appearance_mode("dark")
 
 # setup window
-root = Tk()
+root = ctk.CTk()
 root.title("Documentation Database Interface")
-root.config(bg="skyblue")
 
 photo = PhotoImage(file="search.png")
 root.iconphoto(False, photo)
@@ -108,10 +102,10 @@ db = mysql.connector.connect(**connection_data)
 cursor = db.cursor()
 
 # setup the left and right frames to hold the items
-left_frame = Frame(root, width=200, height=400, bg="grey")
+left_frame = ctk.CTkFrame(root)
 left_frame.grid(row=0, column=0, padx=10, pady=10)
 
-right_frame = Frame(root, width=650, height=400, bg="grey", relief=RAISED)
+right_frame = ctk.CTkScrollableFrame(root, width=930, height=490)
 right_frame.grid(row=0, column=1, padx=10, pady=10)
 
 # placeholder options for the BetterOptionsMenu objects until they get refreshed
@@ -119,80 +113,76 @@ placeholder_options = ["Wating for Connection"]
 
 # add the items in the left frame - SEARCH and ADD NEW
 # SEARCH FRAME
-search_frame = Frame(left_frame, width=175, height=200, bg="pink")
+search_frame = ctk.CTkFrame(left_frame, width=175, height=200)
 search_frame.grid(row=0, column=0, padx=5, pady=5)
 
 #search label
-Label(search_frame, text="Search", bg="pink").grid(row=0, column=0, columnspan=2, padx=5, pady=5)
+ctk.CTkLabel(search_frame, text="Search").grid(row=0, column=0, columnspan=2, padx=5, pady=5)
 
 # entry to search NAME
-search_query = Entry(search_frame, width=30)
+search_query = ctk.CTkEntry(search_frame, width=225)
 search_query.grid(row=1, column=0, columnspan=2)
 
 # checkbox to enable/disable advanced searching
 search_advanced_check = BooleanVar(search_frame)
-Checkbutton(search_frame, text="Advanced Search?", variable=search_advanced_check, bg="pink").grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+ctk.CTkCheckBox(search_frame, text="Advanced Search?", variable=search_advanced_check).grid(row=2, column=0, columnspan=2, padx=5, pady=5)
 
 # checkbox and option menu to allow type searching
 search_advanced_type_check = BooleanVar(search_frame)
-Checkbutton(search_frame, text="Type", variable=search_advanced_type_check, bg="pink").grid(row=3, column=0, padx=5, pady=5)
+ctk.CTkCheckBox(search_frame, text="Type", variable=search_advanced_type_check, width=50).grid(row=3, column=0, padx=5, pady=5)
 
 search_advanced_type = StringVar(search_frame)
-search_advanced_type_menu = BetterOptionMenu(search_frame, search_advanced_type, LstOptions=placeholder_options)
+search_advanced_type_menu = ctk.CTkOptionMenu(search_frame, width=150, variable=search_advanced_type, values=placeholder_options)
 search_advanced_type_menu.grid(row=3, column=1, padx=5, pady=5)
-search_advanced_type_menu.config(width=15)
 
 # checkbox and option menu to allow language searching
 search_advanced_lang_check = BooleanVar(search_frame)
-Checkbutton(search_frame, text="Lang", variable=search_advanced_lang_check, bg="pink").grid(row=4, column=0, padx=5, pady=5)
+ctk.CTkCheckBox(search_frame, text="Lang", variable=search_advanced_lang_check, width=50).grid(row=4, column=0, padx=5, pady=5)
 
 search_advanced_lang = StringVar(search_frame)
-search_advanced_lang_menu = BetterOptionMenu(search_frame, search_advanced_lang, LstOptions=placeholder_options)
+search_advanced_lang_menu = ctk.CTkOptionMenu(search_frame, width=150, variable=search_advanced_lang, values=placeholder_options)
 search_advanced_lang_menu.grid(row=4, column=1, padx=5, pady=5)
-search_advanced_lang_menu.config(width=15)
 
 # search button
-Button(search_frame, text="Search", width=25, command=lambda: search(search_query.get(), search_advanced_check.get(), search_advanced_type_check.get(), search_advanced_type.get(), search_advanced_lang_check.get(), search_advanced_lang.get())).grid(row=5, column=0, columnspan=2, padx=5, pady=5)
+ctk.CTkButton(search_frame, text="Search", width=225, command=lambda: search(search_query.get(), search_advanced_check.get(), search_advanced_type_check.get(), search_advanced_type.get(), search_advanced_lang_check.get(), search_advanced_lang.get())).grid(row=5, column=0, columnspan=2, padx=5, pady=5)
 
 # ADD NEW FRAME
-add_frame = Frame(left_frame, width=175, height=200, bg="pink")
+add_frame = ctk.CTkFrame(left_frame, width=175, height=200)
 add_frame.grid(row=1, column=0, padx=5, pady=5)
 
 #add new label
-Label(add_frame, text="Add New", bg="pink").grid(row=0, column=0, columnspan=2, padx=5, pady=5)
+ctk.CTkLabel(add_frame, text="Add New").grid(row=0, column=0, columnspan=2, padx=5, pady=5)
 
 # data entry frame
-add_data_entry_frame = Frame(add_frame, bg="pink")
+add_data_entry_frame = ctk.CTkFrame(add_frame)
 add_data_entry_frame.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
 
 # name entry
-Label(add_data_entry_frame, text="Name", bg="pink").grid(row=0, column=0, padx=5, pady=5, sticky="W")
-add_name = Entry(add_data_entry_frame, width=22)
+ctk.CTkLabel(add_data_entry_frame, text="Name").grid(row=0, column=0, padx=5, pady=5, sticky="W")
+add_name = ctk.CTkEntry(add_data_entry_frame, width=175)
 add_name.grid(row=0, column=1)
 
 # link entry
-Label(add_data_entry_frame, text="Link", bg="pink").grid(row=1, column=0, padx=5, pady=5, sticky="W")
-add_link = Entry(add_data_entry_frame, width=22)
+ctk.CTkLabel(add_data_entry_frame, text="Link").grid(row=1, column=0, padx=5, pady=5, sticky="W")
+add_link = ctk.CTkEntry(add_data_entry_frame, width=175)
 add_link.grid(row=1, column=1)
 
 # type option menu
-Label(add_data_entry_frame, text="Type", bg="pink").grid(row=2, column=0, padx=5, pady=5, sticky="W")
+ctk.CTkLabel(add_data_entry_frame, text="Type").grid(row=2, column=0, padx=5, pady=5, sticky="W")
 add_type = StringVar(search_frame)
-add_type_menu = BetterOptionMenu(add_data_entry_frame, add_type, LstOptions=placeholder_options)
+add_type_menu = ctk.CTkOptionMenu(add_data_entry_frame, width=175, variable=add_type, values=placeholder_options)
 add_type_menu.grid(row=2, column=1, padx=5, pady=5, sticky="E")
-add_type_menu.config(width=15)
 
 # language option menu
-Label(add_data_entry_frame, text="Lang", bg="pink").grid(row=3, column=0, padx=5, pady=5, sticky="W")
+ctk.CTkLabel(add_data_entry_frame, text="Lang").grid(row=3, column=0, padx=5, pady=5, sticky="W")
 add_lang = StringVar(search_frame)
-add_lang_menu = BetterOptionMenu(add_data_entry_frame, add_lang, LstOptions=placeholder_options)
+add_lang_menu = ctk.CTkOptionMenu(add_data_entry_frame, width=175, variable=add_lang, values=placeholder_options)
 add_lang_menu.grid(row=3, column=1, padx=5, pady=5, sticky="E")
-add_lang_menu.config(width=15)
 
-Button(add_frame, text="Add Lang", width=10, command=lambda: add_new_lang(add_name.get())).grid(row=2, column=0, padx=4, pady=4)
-Button(add_frame, text="Add Type", width=10, command=lambda: add_new_type(add_name.get())).grid(row=2, column=1, padx=4, pady=4)
+ctk.CTkButton(add_frame, text="Add Lang", width=100, command=lambda: add_new_lang(add_name.get())).grid(row=2, column=0, padx=4, pady=4)
+ctk.CTkButton(add_frame, text="Add Type", width=100, command=lambda: add_new_type(add_name.get())).grid(row=2, column=1, padx=4, pady=4)
 
-Button(add_frame, text="Add Link", width=25, command=lambda: add_new_link(add_name.get(), add_link.get(), add_type.get(), add_lang.get())).grid(row=3, column=0, columnspan=2, padx=4, pady=4)
+ctk.CTkButton(add_frame, text="Add Link", width=225, command=lambda: add_new_link(add_name.get(), add_link.get(), add_type.get(), add_lang.get())).grid(row=3, column=0, columnspan=2, padx=4, pady=4)
 
 results_table = Table(right_frame, ["ID", "Link Name", "URL", "Type", "Language"])
 
--- table.py
+++ table.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import webbrowser
+import customtkinter as ctk
 
 class Table:
     def __init__(self, root, columns):
@@ -10,30 +10,30 @@ class Table:
 
     def get_width(self, i):
         if i == 0:
-            return 4
+            return 50
         elif i == 1:
-            return 30
+            return 250
         elif i == 2:
-            return 45
+            return 400
         elif i == 3:
-            return 15
+            return 100
         elif i == 4:
-            return 10
+            return 100
         else:
             return 20
     
     def draw_header(self):
         for i, column in enumerate(self.columns):
             width = self.get_width(i)
-            tk.Label(self.root, text=column, font="SegoeUI 9 bold", bg="grey", width=width, anchor="w").grid(row=0, column=i, padx=5, pady=5)
+            ctk.CTkLabel(self.root, text=column, width=width, anchor="w").grid(row=0, column=i, padx=5, pady=5)
 
     def add_data(self, data):
         for i, row in enumerate(data):
             for j, cell in enumerate(row):
                 width = self.get_width(j)
-                label1 = tk.Entry(self.root, width=width, bg="grey")
+                label1 = ctk.CTkEntry(self.root, width=width)
                 label1.insert(0, cell)
-                label1["state"] = "readonly"
+                label1.configure(state="readonly")
                 label1.grid(row=i+1, column=j, padx=5, pady=5)
 
     def delete_data(self):
