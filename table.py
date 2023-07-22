6fc33d463ec871598d925d6a21fc0b7d4a5ac65b
Ryan Mitcham <ryanmitcham512@gmail.com>
2023-07-22 17:02:59 +0100

    Clickable links

--- table.py
+++ table.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import customtkinter as ctk
+from webbrowser import open
 
 class Table:
     def __init__(self, root, columns):
@@ -25,16 +26,21 @@ class Table:
     def draw_header(self):
         for i, column in enumerate(self.columns):
             width = self.get_width(i)
-            ctk.CTkLabel(self.root, text=column, width=width, anchor="w").grid(row=0, column=i, padx=5, pady=5)
+            ctk.CTkLabel(self.root, text=column, width=width, anchor="w").grid(row=0, column=i, padx=5, pady=5, sticky="W")
 
     def add_data(self, data):
         for i, row in enumerate(data):
             for j, cell in enumerate(row):
                 width = self.get_width(j)
-                label1 = ctk.CTkEntry(self.root, width=width)
-                label1.insert(0, cell)
-                label1.configure(state="readonly")
-                label1.grid(row=i+1, column=j, padx=5, pady=5)
+                if j == 2:
+                    if len(cell) > 60:
+                        link = cell[:60]
+                    else:
+                        link = cell
+                    ctk.CTkButton(self.root, text=link, width=width, command=lambda link=cell: open(link)).grid(row=i+1, column=j, padx=5, pady=5, sticky="EW")
+                else:
+                    ctk.CTkLabel(self.root, text=cell, width=width).grid(row=i+1, column=j, padx=5, pady=5, sticky="W")
+
 
     def delete_data(self):
         for widget in self.root.winfo_children():
